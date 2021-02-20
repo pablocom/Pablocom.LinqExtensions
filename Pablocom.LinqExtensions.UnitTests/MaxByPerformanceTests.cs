51fc0370cd08b159aceaa64d4927fb24858898ff
Pablo <p.c.ramirez5150@gmail.com>
2021-02-20 20:14:32 +0100

    Adds performance tests

---
+++ Pablocom.LinqExtensions.UnitTests/MaxByPerformanceTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Pablocom.LinqExtensions.UnitTests
+{
+    [TestFixture]
+    public class MaxByPerformanceTests
+    {
+        private Random random = new Random();
+        private List<Dummy> sampleCollection = new List<Dummy>();
+        private Stopwatch stopwatch = new Stopwatch();
+
+        public MaxByPerformanceTests()
+        {
+            for (int i = 0; i < 400000000; i++)
+            {
+                sampleCollection.Add(new Dummy(random.Next(-100000, 100000)));
+            }
+        }
+
+        [Test]
+        public void MaxByTests()
+        {
+            GC.Collect();
+            stopwatch.Restart();
+
+            var result = sampleCollection
+                .MaxBy(x => x.IntProperty); // Time complexity: O(n), Space complexity: O(1)
+            
+            Console.WriteLine(stopwatch.Elapsed);
+        }
+        
+        [Test]
+        public void OrderByDescendingAndFirstTests()
+        {
+            GC.Collect();
+            stopwatch.Restart();
+
+            var result = sampleCollection
+                .OrderByDescending(x => x.IntProperty) // Time complexity: O(n*logn), Space complexity: O(log n)
+                .First();
+            
+            Console.WriteLine(stopwatch.Elapsed);
+        }
+    }
+}
--- Pablocom.LinqExtensions.UnitTests/MaxByTests.cs
+++ Pablocom.LinqExtensions.UnitTests/MaxByTests.cs
@@ -4,6 +4,7 @@ using NUnit.Framework;
 
 namespace Pablocom.LinqExtensions.UnitTests
 {
+    [TestFixture]
     public class MaxByTests
     {
         [Test]
